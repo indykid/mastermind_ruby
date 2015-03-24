05cf27962e3b008f9fb061e9703c7f7e7a54c880
jarkyn <jarkyn@ymail.com>
2015-03-24 10:42:11 +0000

    adds more tests and functions in game, unstructured, lack of direction

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,11 @@
 class AI
+  attr_reader :scores
+  def initialize
+    @scores = []
+  end
   def guess
     'rrrr'
   end
-
 end
 
 
@@ -11,8 +14,9 @@ class Game
   WELCOME_MESSAGE = "Welcome to mastermind"
   SCORE_REQUEST   = "Please enter your score"
 
-  def initialize(output, ai)
+  def initialize(output, input, ai)
     self.output = output
+    self.input  = input
     self.ai     = ai
   end
 
@@ -23,11 +27,20 @@ class Game
   def play
     display(ai.guess)
     display(SCORE_REQUEST)
+    input.gets
+  end
+
+  def over?
+    last_score == 4
+  end
+
+  def last_score
+    ai.scores.last
   end
 
   private
 
-  attr_accessor :output, :ai
+  attr_accessor :output, :input, :ai
 
   def display(message)
     output.puts(message)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,8 +2,9 @@ require 'game'
 describe Game do
   it 'displays welcome message at the start' do
     output = StringIO.new
+    input = StringIO.new
     ai = AI.new
-    game = Game.new(output, ai)
+    game = Game.new(output, input, ai)
 
     game.start
 
@@ -13,8 +14,9 @@ describe Game do
   describe '#play' do
     it "displays ai's guess" do
       output = StringIO.new
+      input = StringIO.new
       ai = AI.new
-      game = Game.new(output, ai)
+      game = Game.new(output, input, ai)
 
       game.play
 
@@ -23,12 +25,53 @@ describe Game do
 
     it "asks user for score" do
       output = StringIO.new
+      input = StringIO.new
       ai = AI.new
-      game = Game.new(output, ai)
+      game = Game.new(output, input, ai)
 
       game.play
 
       expect(output.string.chomp).to end_with(Game::SCORE_REQUEST)
     end
+
+    it "receives user's score" do
+      output = StringIO.new
+      input = StringIO.new("2\n1")
+      ai = AI.new
+      game = Game.new(output, input, ai)
+
+      game.play
+
+      expect(input.string.).to eq(game.last_score)
+    end
+  end
+
+  it 'is not over to begin with' do
+    output = StringIO.new
+    input = StringIO.new
+    ai = AI.new
+    game = Game.new(output, input, ai)
+
+    game.start
+
+    expect(game.over?).to be(false)
+  end
+
+  it 'is over if winning score is received' do
+    output = StringIO.new
+    input = StringIO.new("4\n4")
+    ai = AI.new
+    game = Game.new(output, input, ai)
+
+    game.start
+
+    expect(game.over?).to be(true)
   end
 end
+
+
+
+
+
+
+
