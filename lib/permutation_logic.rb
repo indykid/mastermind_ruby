095d4801a5f1b2e4c9bb6a5c543dfb5ee7a337f6
jarkyn <jarkyn@ymail.com>
2015-04-07 18:30:23 +0100

    starts over, initial tests and implementation

--- lib/permutation_logic.rb
+++ lib/permutation_logic.rb
@@ -1,14 +1,13 @@
 class PermutationLogic
 
   def self.generate(options, spaces)
-    if options.length == 1 && spaces == 1
-      [options]
-    elsif options.length == 2 && spaces == 1
-      [[options.first], [options.last]]
 
+    if options.length == 1
+      [0]
     else
-      []
+      [[0], [1]]
     end
+
   end
 
 end
--- spec/permutation_logic_spec.rb
+++ spec/permutation_logic_spec.rb
@@ -1,28 +1,18 @@
 require 'permutation_logic'
 describe PermutationLogic do
   describe ".generate" do
-    it "given one option and no spaces generates no permutations" do
-      options = [0]
-      spaces  = 0
-      expect(PermutationLogic.generate(options, spaces)).to eq([])
-    end
-
     it "given one option and one space generates one permutation" do
       options = [0]
       spaces  = 1
-      expect(PermutationLogic.generate(options, spaces)).to eq([[0]])
+      expect(PermutationLogic.generate(options, spaces)).to eq([0])
     end
 
-    it "given one option and one space generates one permutation" do
-      options = [0]
-      spaces  = 1
-      expect(PermutationLogic.generate(options, spaces)).to eq([[0]])
-    end
-
-    it "given two options and one space returns two permutations" do
+    it "given two options and one space generates 2 permutations" do
       options = [0, 1]
       spaces  = 1
       expect(PermutationLogic.generate(options, spaces)).to eq([[0], [1]])
     end
+
+
   end
 end
