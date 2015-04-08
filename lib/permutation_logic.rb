0d892334e70ac0b2f3c1ab7917ca065dbb2cb9e8
jarkyn <jarkyn@ymail.com>
2015-04-08 11:23:11 +0100

    removes repetition, removes premature argument

--- lib/permutation_logic.rb
+++ lib/permutation_logic.rb
@@ -1,15 +1,11 @@
 class PermutationLogic
 
-  def self.generate(options, spaces)
-
-    if options.length == 1
-      [[0]]
-    elsif options.length == 2
-      [[0], [1]]
-    elsif options.length == 3
-      [[0], [1], [2]]
+  def self.generate(options)
+    result = []
+    options.each do |option|
+      result << [option]
     end
-
+    result
   end
 
 end
--- spec/permutation_logic_spec.rb
+++ spec/permutation_logic_spec.rb
@@ -3,22 +3,18 @@ describe PermutationLogic do
   describe ".generate" do
     it "given one option and one space generates one permutation" do
       options = [0]
-      spaces  = 1
-      expect(PermutationLogic.generate(options, spaces)).to eq([[0]])
+      expect(PermutationLogic.generate(options)).to eq([[0]])
     end
 
     it "given two options and one space generates 2 permutations" do
       options = [0, 1]
-      spaces  = 1
-      expect(PermutationLogic.generate(options, spaces)).to eq([[0], [1]])
+      expect(PermutationLogic.generate(options)).to eq([[0], [1]])
     end
 
     it "given three options and one space generates three permutations" do
       options = [0, 1, 2]
-      spaces  = 1
-      expect(PermutationLogic.generate(options, spaces)).to eq([[0], [1], [2]])
+      expect(PermutationLogic.generate(options)).to eq([[0], [1], [2]])
     end
-
 
   end
 end
