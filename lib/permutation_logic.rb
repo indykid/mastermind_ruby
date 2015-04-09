665f89773774d47ffc48a258300d66662431e498
jarkyn <jarkyn@ymail.com>
2015-04-09 11:12:53 +0100

    adds tests for the case with one option and one space, plus repetitive solution

--- lib/permutation_logic.rb
+++ lib/permutation_logic.rb
@@ -2,5 +2,12 @@ require 'pry'
 class PermutationLogic
 
   def self.generate(options)
+    if options == [0]
+      [[0]]
+    elsif options == [1]
+      [[1]]
+    else
+      [[2]]
+    end
   end
 end
--- spec/permutation_logic_spec.rb
+++ spec/permutation_logic_spec.rb
@@ -1,11 +1,19 @@
 require 'permutation_logic'
 describe PermutationLogic do
   describe ".generate" do
-    it "given one option generates one permutation" do
-      options = [0]
-      expect(PermutationLogic.generate(options)).to eq([[0]])
+    context "given one option and one space generates one permutation" do
+      it "[0]" do
+        options = [0]
+        expect(PermutationLogic.generate(options)).to eq([[0]])
+      end
+      it "[1]" do
+        options = [1]
+        expect(PermutationLogic.generate(options)).to eq([[1]])
+      end
+      it "[2]" do
+        options = [2]
+        expect(PermutationLogic.generate(options)).to eq([[2]])
+      end
     end
-
-
   end
 end
