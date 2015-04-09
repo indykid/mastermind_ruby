6a3574cd60901c5cf64e66deb34e479d68543932
jarkyn <jarkyn@ymail.com>
2015-04-09 12:26:37 +0100

    refactors the code

--- lib/permutation_logic.rb
+++ lib/permutation_logic.rb
@@ -2,14 +2,10 @@ require 'pry'
 class PermutationLogic
 
   def self.generate(options)
-    if options.length == 1
-      [options]
-    else
-      result = []
-      options.each do |option|
-        result << [option]
-      end
-      result
+    result = []
+    options.each do |option|
+      result << [option]
     end
+    result
   end
 end
