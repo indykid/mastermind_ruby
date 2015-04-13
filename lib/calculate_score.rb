5bb9f1fe770aeb646e02df00fa5a85f57f59dc5d
jarkyn <jarkyn@ymail.com>
2015-04-13 17:08:57 +0100

    introduces identical duplication for easy removal

--- lib/calculate_score.rb
+++ lib/calculate_score.rb
@@ -1,8 +1,8 @@
 class CalculateScore
   def self.corrects(code, guess)
-    return 2 if code == [2, 0] && guess == [0, 2]
-    return 2 if code == [3, 1] && guess == [1, 3]
-    return 2 if code == [4, 5] && guess == [5, 4]
+    return 2 if code[0] == guess[1] && guess[0] == code[1]
+    return 2 if code[0] == guess[1] && guess[0] == code[1]
+    return 2 if code[0] == guess[1] && guess[0] == code[1]
     return code.length if code == guess
     0
   end
