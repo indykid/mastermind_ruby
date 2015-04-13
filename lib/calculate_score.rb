fc5d3e1dea90f5a95f052064f393aeffe4ca6bed
jarkyn <jarkyn@ymail.com>
2015-04-13 14:23:20 +0100

    removes duplication for implementation of correct score for arrays of size 1

--- lib/calculate_score.rb
+++ lib/calculate_score.rb
@@ -1,8 +1,6 @@
 class CalculateScore
   def self.corrects(code, guess)
-    return 1 if code == [0] && guess == [0]
-    return 1 if code == [1] && guess == [1]
-    return 1 if code == [2] && guess == [2]
+    return 1 if code == guess
     0
   end
 end
