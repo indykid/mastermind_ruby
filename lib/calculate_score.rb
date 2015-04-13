300e6ba9ea2b14237a7c681e4b37a4744267fb55
jarkyn <jarkyn@ymail.com>
2015-04-13 16:42:57 +0100

    replaces constant with a variable

--- lib/calculate_score.rb
+++ lib/calculate_score.rb
@@ -1,6 +1,6 @@
 class CalculateScore
   def self.corrects(code, guess)
-    return 3 if code == guess && code.length == 3
+    return code.length if code == guess && code.length == code.length
     return 2 if code == guess && code.length == 2
     return 1 if code == guess && code.length == 1
     0
