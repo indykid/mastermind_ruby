7f56b968cae2a876a900dee89f9c3ee250edd8c8
jarkyn <jarkyn@ymail.com>
2015-04-13 17:11:30 +0100

    removes duplication

--- lib/calculate_score.rb
+++ lib/calculate_score.rb
@@ -1,8 +1,6 @@
 class CalculateScore
   def self.corrects(code, guess)
     return 2 if code[0] == guess[1] && guess[0] == code[1]
-    return 2 if code[0] == guess[1] && guess[0] == code[1]
-    return 2 if code[0] == guess[1] && guess[0] == code[1]
     return code.length if code == guess
     0
   end
