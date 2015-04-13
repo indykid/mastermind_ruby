58d6eb5eee2ce160780fbe2589ebf12c653504c9
jarkyn <jarkyn@ymail.com>
2015-04-13 18:03:27 +0100

    removes redundant code

--- lib/calculate_score.rb
+++ lib/calculate_score.rb
@@ -1,10 +1,8 @@
 class CalculateScore
   def self.corrects(code, guess)
-    return 1 if code == [1, 4] && guess == [3, 1]
-    return 1 if code == [2, 5] && guess == [2, 1]
-    return 1 if code == [3, 5] && guess == [0, 3]
+
     difference = code - guess
     return code.length - difference.length
-    0
+
   end
 end
