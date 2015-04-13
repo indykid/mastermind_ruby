0b1900c56038e6751b9fff0e4c6ed0148a00c8c6
jarkyn <jarkyn@ymail.com>
2015-04-13 17:59:34 +0100

    changes the logic from being based on how many elements in common to how many elements are not common

--- lib/calculate_score.rb
+++ lib/calculate_score.rb
@@ -3,7 +3,8 @@ class CalculateScore
     return 1 if code == [1, 4] && guess == [3, 1]
     return 1 if code == [2, 5] && guess == [2, 1]
     return 1 if code == [3, 5] && guess == [0, 3]
-    return code.length if code.sort == guess.sort
+    difference = code - guess
+    return code.length - difference.length
     0
   end
 end
