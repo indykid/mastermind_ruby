587c71b0e442dd27e3ce959b60691ee33fb5cdc2
jarkyn <jarkyn@ymail.com>
2015-04-13 16:44:07 +0100

    removes reduntant duplication

--- lib/calculate_score.rb
+++ lib/calculate_score.rb
@@ -1,8 +1,6 @@
 class CalculateScore
   def self.corrects(code, guess)
     return code.length if code == guess
-    return 2 if code == guess && code.length == 2
-    return 1 if code == guess && code.length == 1
     0
   end
 end
