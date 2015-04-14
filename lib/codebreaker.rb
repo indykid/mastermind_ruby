7a4763dd2e825cf90c51b66a58b6c168dd736a0f
jarkyn <jarkyn@ymail.com>
2015-04-14 08:11:02 +0100

    adds test and implementation for the initial guess

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,14 +1,39 @@
 class Codebreaker
-  attr_reader :guesses, :corrects, :exacts
-  attr_accessor :guesses, :exacts
+  attr_reader :colours
+  attr_accessor :corrects, :guesses, :exacts
 
   def initialize
     @guesses = []
     @corrects = []
     @exacts = []
+    @colours = ['r', 'g', 'b', 'y', 'p', 'o']
   end
 
   def guess
-    @guesses << 'rrrg'
+    if first_time?
+      guess = first_guess
+    else
+      guess == ['r', 'r', 'g', 'y']
+    end
+    @guesses << guess
+    guess
   end
+
+  private
+
+  def first_guess
+    first_colour  = colours.sample
+    second_colour = colours.sample
+    while second_colour == first_colour
+      second_colour = colours.sample
+    end
+    return [first_colour, first_colour, second_colour, second_colour]
+  end
+
+  def first_time?
+    guesses.length == 0
+  end
+
+
+
 end
